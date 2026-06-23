a48f9d10634a31d977c807a0f2b6ccb98091503c
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-06-23 20:08:23 +0100

    Python: Loops

---
+++ loops.py
@@ -0,0 +1,6 @@
+for i in range(5):
+    print("Hello, World!") # Print a message 5 times; i means count up to 4
+    
+# Count by 2s
+for i in range(0, 10, 2):
+    print(i) # Print even numbers from 0 to 8
