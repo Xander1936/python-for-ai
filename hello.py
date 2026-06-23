0b517b78c0a3974c935d88da517048477af216b3
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2026-06-23 19:24:08 +0100

    Python: Variables and DataTypes

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+# import requests
+
+# response = requests.get('https://api.github.com')
+# print(response.status_code)
+# print("Hello, World!")
+
+
+
+first_name = "Alice"
+last_name = "Smith"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+long_dash = "-" * 12
+
+print(long_dash)
+
+user_age = 30
+
+print(f"Hello, {first_name}! You are {user_age} years old.")
+length_of_name = len(full_name)
+print(f"Your full name has {length_of_name} characters.")
+
+age = 30
+has_license = True
+drunk = True
+
+can_drive = age >= 18 and has_license and not drunk
+print(f"Can {first_name} drive? {can_drive}")
